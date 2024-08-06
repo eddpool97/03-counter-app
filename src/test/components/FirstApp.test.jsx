a8b438b45c1f09bb1033087bbcf41e9cb7b2cf76
eddpool97 <edwin.e.ayala1997@gmail.com>
2024-08-06 17:03:35 -0600

    Configuracion de archivos para testing - Fallido

--- src/test/components/FirstApp.test.jsx
+++ src/test/components/FirstApp.test.jsx
@@ -4,6 +4,6 @@ import { FirstApp } from "../../FirstApp";
 
 describe('Pruebas en <FirstApp />' ,()=>{
     test('Debe de hacer match con el snapshot', ()=>{
-        render(<FirstApp />);
+        render(<FirstApp title="First App Running" message="Hello Edwin" name="Edwin Ayala"/>);
     });
 });
