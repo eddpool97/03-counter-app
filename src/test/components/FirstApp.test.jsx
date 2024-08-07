bc8d7e0399f8e82c45ef4ebbad551e4db02fa1dc
Edwin Ayala <edwin.e.ayala1997@gmail.com>
2024-08-06 22:18:22 -0600

    Primeras pruebas y snapshots del componente <FirstApp />

--- src/test/components/FirstApp.test.jsx
+++ src/test/components/FirstApp.test.jsx
@@ -4,6 +4,23 @@ import { FirstApp } from "../../FirstApp";
 
 describe('Pruebas en <FirstApp />' ,()=>{
     test('Debe de hacer match con el snapshot', ()=>{
-        render(<FirstApp title="First App Running" message="Hello Edwin" name="Edwin Ayala"/>);
+        const {container} = render(<FirstApp title="First App Running" message="Hello Edwin" name="Edwin Ayala"/>);
+
+        expect( container ).toMatchSnapshot();
+
+    });
+
+    test('Debe de mostrar el titulo en H1', ()=>{
+
+        const title = 'First App Running';
+
+        const {container, getByText } = render(<FirstApp title="First App Running" message="Hello Edwin" name="Edwin Ayala"/>);
+
+        expect( getByText(title) ).toBeTruthy();
+
+        const h1 = container.querySelector('h1');
+        console.log( h1.innerHTML );
+
+        expect( container ).toMatchSnapshot();
     });
 });
